7f02f64e87a2aaddaef2053e6fec9774e840354f
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-10-19 02:11:31 -0600

    Pass Habit to ViewHabitFragment
    I first thought about using tags to pass data but since we're already
    using ViewModels, it would be more consistent to use them.
    
    We first need to find a way to select a `Habit` to view. This is done by
    creating an 'onClick' interface for the `HabitAdapter` which is
    implemented by the `TodaysHabitsFragment`. The `TodaysHabitsFragment`
    selects the habit while the `ViewHabitFragment` observes for changes to
    the `LiveData` and updates the UI accordingly.

---
+++ app/src/main/java/com/github/cmput301f21t44/hellohabits/view/ViewHabitFragment.java
@@ -0,0 +1,64 @@
+package com.github.cmput301f21t44.hellohabits.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.github.cmput301f21t44.hellohabits.R;
+import com.github.cmput301f21t44.hellohabits.databinding.FragmentViewHabitBinding;
+import com.github.cmput301f21t44.hellohabits.viewmodel.SelectedHabitViewModel;
+
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class ViewHabitFragment extends Fragment {
+    private FragmentViewHabitBinding binding;
+    private SelectedHabitViewModel mViewModel;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentViewHabitBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // attach the provider to activity instead of fragment so the fragments can share data
+        mViewModel = new ViewModelProvider(requireActivity())
+                .get(SelectedHabitViewModel.class);
+
+        binding.buttonBackToList.setOnClickListener(v->
+                NavHostFragment
+                        .findNavController(ViewHabitFragment.this)
+                        .navigate(R.id.action_viewHabitFragment_to_todaysHabitsFragment));
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mViewModel.getSelected().observe(getViewLifecycleOwner(), habit-> {
+            // update UI
+            binding.viewTitle.setText(habit.getTitle());
+            binding.viewReason.setText( habit.getReason());
+            String date = DateTimeFormatter
+                    .ofPattern("eeee d MMMM, y")
+                    .withZone(ZoneId.systemDefault())
+                    .format(habit.getDateStarted());
+            binding.viewDateStarted.setText(date);
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}

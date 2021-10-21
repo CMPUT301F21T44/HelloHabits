8a70af742bf6f5dacfe3612293a717f708422cab
Chenxuan Wang <cxleoo9@gmail.com>
2021-10-20 22:44:57 -0600

    feature delete-habit

--- app/src/main/java/com/github/cmput301f21t44/hellohabits/db/HabitDao.java
+++ app/src/main/java/com/github/cmput301f21t44/hellohabits/db/HabitDao.java
@@ -2,8 +2,11 @@ package com.github.cmput301f21t44.hellohabits.db;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+
+import com.github.cmput301f21t44.hellohabits.model.Habit;
 
 import java.util.List;
 
@@ -17,4 +20,8 @@ public interface HabitDao {
 
     @Query("DELETE FROM habits")
     void deleteAll();
+
+    @Delete
+    void delete(HabitEntity habit);
+
 }

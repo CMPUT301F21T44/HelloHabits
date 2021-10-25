614930ab5879b5ec9044591f3ad438e7a615a16f
Mark Jia <mjia@ualberta.ca>
2021-10-25 14:58:24 -0600

    View current day's Habits
    - Make Habit.isInDay() in Habit.java
    - Implement the checkBox feature in TodayHabitsFragment.java
    
    Closes #7

--- app/src/main/java/com/github/cmput301f21t44/hellohabits/model/Habit.java
+++ app/src/main/java/com/github/cmput301f21t44/hellohabits/model/Habit.java
@@ -4,13 +4,13 @@ import java.time.Instant;
 import java.util.List;
 
 public interface Habit {
-    static final int MONDAY = 0;
-    static final int TUESDAY = 1;
-    static final int WEDNESDAY = 2;
-    static final int THURSDAY = 3;
-    static final int FRIDAY = 4;
-    static final int SATURDAY = 5;
-    static final int SUNDAY = 6;
+    static final int MONDAY = 1;
+    static final int TUESDAY = 2;
+    static final int WEDNESDAY = 3;
+    static final int THURSDAY = 4;
+    static final int FRIDAY = 5;
+    static final int SATURDAY = 6;
+    static final int SUNDAY = ;0
 
     String getId();
 
@@ -21,5 +21,20 @@ public interface Habit {
     Instant getDateStarted();
 
     List<HabitEvent> getEvents();
+
     boolean[] getDaysOfWeek();
+
+    static boolean isInDay(Instant today, boolean[] dayOfWeek) {
+        // dayOfWeek goes from 1-7 (Monday-Sunday)
+        int dayOfWeek = today.atZone(ZoneId.systemDefault()).getDayOfWeek().getValue();
+
+        // daysOfWeek is an array that matches the checkBox for viewing.
+        // in case today is Monday:
+        // [true, true ,true, true, true, true, true] = every day
+        // [true, false , false, false, false, false, false] = only on monday
+        // [true, false , false, false, false, false, true] = for monday and sunday 
+        return daysOfWeek[dayOfWeek-1]
+    }
+
+
 }

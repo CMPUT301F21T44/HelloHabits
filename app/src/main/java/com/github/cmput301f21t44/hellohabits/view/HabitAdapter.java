5c043247e146177dc97cd556fdcecf7d28f877e1
ashutoshlamba <55332461+ashutoshlamba@users.noreply.github.com>
2021-10-26 15:58:50 -0600

    added consistency check
    program can now assign an icon to the habit based on its level of consistency, if habit doesnt repeat or hasnt started, consistency is 100%

--- app/src/main/java/com/github/cmput301f21t44/hellohabits/view/HabitAdapter.java
+++ app/src/main/java/com/github/cmput301f21t44/hellohabits/view/HabitAdapter.java
@@ -12,8 +12,14 @@ import com.github.cmput301f21t44.hellohabits.R;
 import com.github.cmput301f21t44.hellohabits.databinding.LlistHabitItemBinding;
 import com.github.cmput301f21t44.hellohabits.model.Habit;
 
+import java.time.DayOfWeek;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.EnumSet;
+import java.util.Set;
 
 public class HabitAdapter extends ListAdapter<Habit, HabitAdapter.HabitHolder> {
     private final OnItemClickListener<Habit> listener;
@@ -63,7 +69,50 @@ public class HabitAdapter extends ListAdapter<Habit, HabitAdapter.HabitHolder> {
         void bind(@NonNull final Habit habit, final OnItemClickListener<Habit> listener) {
             mItemBinding.titleView.setText(habit.getTitle());
             mItemBinding.reasonView.setText(habit.getReason());
-            mItemBinding.imageView.setImageResource(R.mipmap.green_logo_round);
+            // check level of consistency
+            Instant instant = habit.getDateStarted();
+            LocalDate startDate = instant.atZone(ZoneId.of( "America/Edmonton" )).toLocalDate();
+            LocalDate endDate = LocalDate.now();
+            int startDay = startDate.getDayOfWeek().getValue();
+            int endDay = endDate.getDayOfWeek().getValue();
+            long days = ChronoUnit.DAYS.between(startDate, endDate);
+            boolean recurranceDays[] = habit.getDaysOfWeek();
+            int numRecurranceDays = 0;
+            for (int i = 0;i<recurranceDays.length;i++){
+                if(recurranceDays[i]){
+                    numRecurranceDays++;
+                }
+            }
+            long totalDays = 0;
+            for (int i = startDay;i<=7;i++){
+                if (recurranceDays[i-1]){
+                    totalDays++;
+                }
+                days--;
+            }
+            for(int i = endDay;i>=1;i--){
+                if (recurranceDays[i-1]){
+                    totalDays++;
+                }
+                days--;
+            }
+            totalDays = totalDays + numRecurranceDays*days/7;
+            double consistency = 1;
+            if(totalDays!=0) {
+                consistency = habit.getEvents().size() / totalDays;
+            }
+            // if consistency <50% red logo, if between 50 and 75%, yellow logo,
+            // if greater than 75% green logo
+            if(consistency<0.5){
+                mItemBinding.imageView.setImageResource(R.mipmap.red_logo_round);
+            }
+            else if(consistency<0.75){
+                mItemBinding.imageView.setImageResource(R.mipmap.yellow_logo_round);
+            }
+            else{
+                mItemBinding.imageView.setImageResource(R.mipmap.green_logo_round);
+            }
+
             mItemBinding.getRoot().setOnClickListener(v-> listener.onItemClick(habit));
         }
 

66b750632ac5e486226fb50683e3092971f4de06
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-11-05 14:41:00 -0600

    Add unit test for DaysOfWeek

---
+++ app/src/test/java/com/github/cmput301f21t44/hellohabits/HabitTest.java
@@ -0,0 +1,11 @@
+package com.github.cmput301f21t44.hellohabits;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+@Ignore("Eh")
+public class HabitTest {
+
+}

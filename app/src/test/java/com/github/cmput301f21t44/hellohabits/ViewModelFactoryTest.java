c00a531d6f1d66b456eee3361af6629280d7283a
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-11-16 18:45:09 -0700

    Create ViewModelFactory tests
    The rest of the methods kind of need a real instance so no need to test

---
+++ app/src/test/java/com/github/cmput301f21t44/hellohabits/ViewModelFactoryTest.java
@@ -0,0 +1,58 @@
+package com.github.cmput301f21t44.hellohabits;
+
+import static org.junit.Assert.assertNotNull;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+
+import com.github.cmput301f21t44.hellohabits.model.HabitEventRepository;
+import com.github.cmput301f21t44.hellohabits.model.HabitRepository;
+import com.github.cmput301f21t44.hellohabits.viewmodel.HabitEventViewModel;
+import com.github.cmput301f21t44.hellohabits.viewmodel.HabitViewModel;
+import com.github.cmput301f21t44.hellohabits.viewmodel.PreviousListViewModel;
+import com.github.cmput301f21t44.hellohabits.viewmodel.ViewModelFactory;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+@RunWith(JUnit4.class)
+public class ViewModelFactoryTest {
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+
+    /**
+     * Mock HabitRepository object to be injected to ViewModelFactory
+     */
+    @Mock
+    HabitRepository mockHabitRepo;
+
+    /**
+     * Mock HabitEventRepository object to be injected to ViewModelFactory
+     */
+    @Mock
+    HabitEventRepository mockHabitEventRepo;
+
+    private ViewModelFactory viewModelFactory;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        viewModelFactory = new ViewModelFactory(mockHabitRepo, mockHabitEventRepo);
+    }
+
+    @Test
+    public void test_isDefined() {
+        assertNotNull(viewModelFactory);
+    }
+
+    @Test
+    public void test_createViewModels() {
+        assertNotNull(viewModelFactory.create(HabitViewModel.class));
+        assertNotNull(viewModelFactory.create(HabitEventViewModel.class));
+        assertNotNull(viewModelFactory.create(PreviousListViewModel.class));
+    }
+}

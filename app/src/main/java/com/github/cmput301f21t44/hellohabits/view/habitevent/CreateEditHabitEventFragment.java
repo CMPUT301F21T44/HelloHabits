5a167bf5cbc90637efbf8004a3f354a6590c1045
Mark Jia <mjia@ualberta.ca>
2021-11-28 03:55:42 -0700

    add some methods about photos

--- app/src/main/java/com/github/cmput301f21t44/hellohabits/view/habitevent/CreateEditHabitEventFragment.java
+++ app/src/main/java/com/github/cmput301f21t44/hellohabits/view/habitevent/CreateEditHabitEventFragment.java
@@ -1,12 +1,22 @@
 package com.github.cmput301f21t44.hellohabits.view.habitevent;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
@@ -20,6 +30,8 @@ import com.github.cmput301f21t44.hellohabits.viewmodel.HabitEventViewModel;
 import com.github.cmput301f21t44.hellohabits.viewmodel.HabitViewModel;
 import com.github.cmput301f21t44.hellohabits.viewmodel.ViewModelFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Objects;
 
 /**
@@ -33,6 +45,10 @@ public class CreateEditHabitEventFragment extends Fragment {
     private HabitEvent mHabitEvent;
     private boolean isEdit;
     private NavController mNavController;
+    public static final int REQUEST_CODE_CAM = 1;
+    public static final int REQUEST_CODE_GLY = 0;
+    private Uri imageUri;
+    private String imageBase64;
 
     /**
      * When the view is created, connect the layout to the class using binding
@@ -139,7 +155,8 @@ public class CreateEditHabitEventFragment extends Fragment {
         binding.buttonAddHabitEvent.setOnClickListener(v -> submitHabitEvent());
         binding.buttonAddLocation.setOnClickListener(v -> getLocation());
 
-        binding.buttonAddPhoto.setOnClickListener(v -> getPhoto());
+        binding.buttonCamera.setOnClickListener(v -> getFromCamera());
+        binding.buttonGallery.setOnClickListener(v -> getFromGallery());
     }
 
     /**
@@ -150,12 +167,78 @@ public class CreateEditHabitEventFragment extends Fragment {
         Toast.makeText(getActivity(), "Not implemented yet!", Toast.LENGTH_SHORT).show();
     }
 
+
+
     /**
-     * Get a photo to attach to the event
+     * Get a photo to attach to the event by taking a photo from camera
+     */
+    private void getFromCamera() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            // GOTO take photo if has permission
+            doTkPho();
+        } else {
+            // ask for permission
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.CAMERA}, REQUEST_CODE_CAM);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                doTkPho();
+            } else {
+                Toast.makeText(requireContext(), "We have no permission to camera! (T▽T) ...", Toast.LENGTH_SHORT).show();
+            }
+        } else if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                doChsGly();
+            } else {
+                Toast.makeText(requireContext(), "We have no permission to gallery! (T▽T) ...", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private String mPicName = "imageOut.jpeg";
+    private void doTkPho() {
+        File imageTemp = new File(requireActivity().getExternalCacheDir(), mPicName);
+        if (imageTemp.exists()){
+            imageTemp.delete();
+            //TODO: Instead of deleting the file, change the file name (relative to datetime)
+        }
+        try {
+            imageTemp.createNewFile();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        if (Build.VERSION.SDK_INT > 24) {
+            // contentProvider
+            imageUri = FileProvider.getUriForFile(requireContext(), "github.cmput301f21t44.hellohabits.fileprovider", imageTemp);
+        } else {
+            imageUri = Uri.fromFile(imageTemp);
+        }
+        Intent intent = new Intent();
+        intent.setAction("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, REQUEST_CODE_CAM);
+
+    }
+
+
+
+    /**
+     * Get a photo to attach to the event by choosing photo from gallery
      * TODO: Implement
      */
-    private void getPhoto() {
+    private void getFromGallery() {
         Toast.makeText(getActivity(), "Not implemented yet!", Toast.LENGTH_SHORT).show();
+    }
+
+    private void doChsGly(){
+
     }
 
     /**
